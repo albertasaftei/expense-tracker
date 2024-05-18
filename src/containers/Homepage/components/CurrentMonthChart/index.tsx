f0ede05fc04ad6da251b972d43d9b1a635d0712d
alburt <albert.asaftei00@gmail.com>
2024-05-18 12:52:50 +0200

    ♻️ refactor: moved chart's labels on the outside because not visible

--- src/containers/Homepage/components/CurrentMonthChart/index.tsx
+++ src/containers/Homepage/components/CurrentMonthChart/index.tsx
@@ -5,7 +5,6 @@ import { Cell, Pie, PieChart, ResponsiveContainer } from "recharts";
 import { RootState } from "src/store";
 import colors from "src/utils/colors";
 
-const RADIAN = Math.PI / 180;
 const COLORS = [colors.lightRed, colors.lightGreen, "#A3D9FF", "#FFB627"];
 
 const CurrentMonthChart: React.FC = () => {
@@ -29,38 +28,8 @@ const CurrentMonthChart: React.FC = () => {
             cx="50%"
             cy="50%"
             fill={colors.lightRed}
-            labelLine={false}
-            label={({
-              categoryId,
-              cx,
-              cy,
-              midAngle,
-              innerRadius,
-              outerRadius,
-            }) => {
-              const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-              const x = cx + radius * Math.cos(-midAngle * RADIAN);
-              const y = cy + radius * Math.sin(-midAngle * RADIAN);
-
-              return (
-                <text
-                  x={x}
-                  y={y}
-                  fill="white"
-                  textAnchor={x > cx ? "start" : "middle"}
-                  dominantBaseline="central"
-                  xlinkHref={categories[categoryId]?.iconUrl}
-                >
-                  <img
-                    src={categories[categoryId]?.iconUrl}
-                    style={{
-                      width: 20,
-                      height: 20,
-                    }}
-                  />
-                  {`${categories[categoryId]?.name}`}
-                </text>
-              );
+            label={({ categoryId }) => {
+              return `${categories[categoryId]?.name}`;
             }}
           >
             {expensesCurrentMonth.map((_, index) => (
